2f8116e4273b39ece164b17887c74da1d5d33280
apu <pasha_anik@ukr.net>
2018-09-06 19:03:28 +0300

    Add info for starting loading database

--- SQL-scripts/mysql_insert_data.sql
+++ SQL-scripts/mysql_insert_data.sql
@@ -4,7 +4,12 @@ INSERT INTO user(user_id, first_name, second_name, phone_number, email) VALUES(1
 
 INSERT INTO card(card_id, card_number, user_id) VALUES(1, "11111111", 1);
 
-INSERT INTO device(device_id, device_number, last_packet_id) VALUES(1, 15, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(1, 11, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(2, 12, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(3, 13, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(4, 14, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(5, 15, 1);
+INSERT INTO device(device_id, device_number, last_packet_id) VALUES(6, 16, 1);
 
 INSERT INTO event_type VALUES(1,"ACCESS_ALLOW");
 INSERT INTO event_type VALUES(2,"ACCESS_DENIED");
@@ -14,3 +19,8 @@ INSERT INTO event_type VALUES(4,"EXIT_QUERY");
 INSERT INTO rule_type VALUES(1, "ENTER");
 
 INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 1, 3, 1, null, null);
+INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 2, 3, 1, null, null);
+INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 3, 3, 1, null, null);
+INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 4, 3, 1, null, null);
+INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 5, 3, 1, null, null);
+INSERT INTO rule(card_id, device_id, event_id, rule_type_id, date_begin, date_end) VALUES(1, 6, 3, 1, null, null);
