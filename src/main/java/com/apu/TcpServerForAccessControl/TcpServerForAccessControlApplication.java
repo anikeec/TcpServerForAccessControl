c10e16f2dbc6b3cf0259ac85ba232595349ef271
apu <pasha_anik@ukr.net>
2018-09-06 19:01:04 +0300

    Move entities and repositories to separate project for ease using them

--- src/main/java/com/apu/TcpServerForAccessControl/TcpServerForAccessControlApplication.java
+++ src/main/java/com/apu/TcpServerForAccessControl/TcpServerForAccessControlApplication.java
@@ -6,12 +6,20 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.apu.TcpServerForAccessControl.utils.logging.LoggingOutputStream;
 
 @SpringBootApplication
+//(scanBasePackages = {"com.apu.TcpServerForAccessControl.utils", "com.apu.TcpServerForAccessControlDB.entity"})
+//@EnableJpaRepositories(basePackageClasses=com.apu.TcpServerForAccessControlDB.repository.AccessMessageRepository.class)
+//@EnableAutoConfiguration
+@EnableJpaRepositories(basePackages= {"com.apu.TcpServerForAccessControlDB.repository"})
+@EntityScan(basePackages = {"com.apu.TcpServerForAccessControlDB.entity"})  
 @ImportResource("/META-INF/spring/integration/integration.xml")
 public class TcpServerForAccessControlApplication {
     
