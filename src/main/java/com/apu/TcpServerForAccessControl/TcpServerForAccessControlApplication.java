5827689919db0a4045e9fd176c9bcbe65ce1f5a6
apu <pasha_anik@ukr.net>
2018-09-12 15:18:01 +0300

    Begin adding cache to server and this allow to decrease answer timeout (without saving to database, only reading) from 25-30 ms to 3-7 ms.

--- src/main/java/com/apu/TcpServerForAccessControl/TcpServerForAccessControlApplication.java
+++ src/main/java/com/apu/TcpServerForAccessControl/TcpServerForAccessControlApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.apu.TcpServerForAccessControl.utils.logging.LoggingOutputStream;
 
 @SpringBootApplication
+//@EnableCaching
 //(scanBasePackages = {"com.apu.TcpServerForAccessControl.utils", "com.apu.TcpServerForAccessControlDB.entity"})
 //@EnableJpaRepositories(basePackageClasses=com.apu.TcpServerForAccessControlDB.repository.AccessMessageRepository.class)
 //@EnableAutoConfiguration
